6eee11ff0aa7fd49ff88752c9f15809ff3bf6b17
Jaya Bajaj <jaya.bajaj@volansystech.com>
2016-11-04 11:47:47 +0530

    commit 2

--- travelbuzz-app/src/app/home/home.component.ts
+++ travelbuzz-app/src/app/home/home.component.ts
@@ -1,9 +1,22 @@
 import { Component } from '@angular/core';
+import { User } from './user.interface';
 
 @Component({
   selector: 'home',
   templateUrl: './home.component.html',
-//   styleUrls: ['./app.component.css']
+  //   styleUrls: ['./app.component.css']
 })
 export class HomeComponent {
+  public user: User;
+
+  public trip = [
+    { value: 'single-trip', display: 'Single Trip' },
+    { value: 'round-trip', display: 'Round Trip' }
+  ];
+
+  ngOnInit(){
+    this.user = {
+      trip: this.trip[0].value
+    }
+  }
 }
